07b752cfebe3d887be15bbb3e1089cb6ee63d5e5
irynasmile <73856579+irynasmile@users.noreply.github.com>
2021-05-14 14:06:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,14 +29,38 @@ if (existingTheme) {
 
   themeSwitchEl.addEventListener('change', changeTheme);
 
+// function classListChange(){};
+
   function changeTheme(e){
-    if(e.target.checked){
-      document.body.classList.remove(existingTheme);
-      document.body.classList.add('dark-theme');
+    if(e.target.checked)
+   {
+      document.body.classList.toggle('dark-theme');
+      // document.body.classList.toggle('light-theme');
       localStorage.setItem('Theme', JSON.stringify(Theme.DARK));
-    }else{
-      document.body.classList.remove(existingTheme);
-      document.body.classList.remove('dark-theme');
+    }
+    else
+    {
+      document.body.classList.toggle('light-theme');
+      document.body.classList.toggle('dark-theme');
       localStorage.setItem('Theme', JSON.stringify(Theme.LIGHT));
     }
 };
+
+
+
+
+
+
+
+
+//   function changeTheme(e){
+//     if(e.target.checked){
+//       document.body.classList.remove(existingTheme);
+//       document.body.classList.add('dark-theme');
+//       localStorage.setItem('Theme', JSON.stringify(Theme.DARK));
+//     }else{
+//       document.body.classList.remove(existingTheme);
+//       document.body.classList.remove('dark-theme');
+//       localStorage.setItem('Theme', JSON.stringify(Theme.LIGHT));
+//     }
+// };
